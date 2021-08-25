3c70220cb3b9d24b4e034e08039977e54541dd91
Muhammed Moussa <mohamedmostafa@paymob.com>
2021-08-25 17:27:25 +0200

    fix(http): modify headers
    added missed headers && handle "X-Paymob-SDK-Agent" header right way

--- src/http/_requesAgenttHeadersBuilder.js
+++ src/http/_requesAgenttHeadersBuilder.js
@@ -3,17 +3,30 @@ const os = require("os");
 
 export const requesAgenttHeadersBuilder = () => {
 	const userAgent = `Paymob Node SDK ${getApiVersion()}`;
+	const underlyingSysInfoCollector = () => {
+		const system = os.type();
+		const node = JSON.stringify(os.cpus());
+		const release = os.release();
+		const version = os.version();
+		const machine = os.arch();
+
+		const underlyingSysInfo = `(system='${system}', node='${node}', release='${release}', version='${version}', machine='${machine}')`;
+		return underlyingSysInfo;
+	};
+
 	const userAgentObj = {
 		"sdk_api_version": getApiVersion(),
 		"sdk_language": "Node",
 		"sdk_authority": "Paymob",
 		"sdk_lang_version": process.version,
 		"sdk_platform": os.platform(),
+		"sdk_request_client": "Node",
+		"sdk_underlying_sys_info": underlyingSysInfoCollector()
 	};
 
 	const headers = {
 		"User-Agent": userAgent,
-		...userAgentObj
+		"X-Paymob-SDK-Agent": JSON.stringify(userAgentObj)
 	};
 
 	return headers;
