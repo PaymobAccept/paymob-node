b734b6f6c939c4054757f6a377d06a99d29a930b
Muhammed Moussa <mohamedmostafa@paymob.com>
2021-08-26 13:35:20 +0200

    docs(paymobfactory): jsdoc
    document PaymobFactory class

--- src/Paymob.js
+++ src/Paymob.js
@@ -1,14 +1,26 @@
 import { Intention } from "./intention";
 import { paymobLogger } from "./utils";
 
+/**
+ * Paymob Node SDK factory
+ * @class PaymobFactory
+ */
+
 class PaymobFactory {
+	/**
+	 * Creates an instance of PaymobFactory.
+	 * @param {*} secretKey
+	 * @memberof PaymobFactory
+	 */
 	constructor(secretKey) {
 		this.secretKey = secretKey;
 		this._intention = new Intention(this.secretKey);
 		this._initilizeIntentionBuilder();
 		paymobLogger(`Paymob Node SDK Initialized with secret Key: ${this.secretKey}`);
 	}
-	
+
+
+	/** @private */
 	_initilizeIntentionBuilder() {
 		this.Intention = {
 			create: (payload) => this._intention.create(payload),
