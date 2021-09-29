120cbc518c630e0b6a8381fe419e16342706bd62
Muhammed Moussa <mohamedmostafa@paymob.com>
2021-09-29 19:28:20 +0200

    fix(utils): remove un-resolved module

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,9 @@
 import {getApiBaseUrl} from "./_getApiBaseUrl";
 import {getApiVersion} from "./_getApiVersion";
-import {httpPromise} from "./_httpPromise";
 import {paymobLogger} from "./_paymobLogger";
 
 export {
 	getApiBaseUrl,
 	getApiVersion,
-	httpPromise,
 	paymobLogger
 };
