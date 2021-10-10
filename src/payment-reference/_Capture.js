54fd18d5e47047114d7f4052dad6fdddba6c8d13
Muhammed Moussa <mohamedmostafa@paymob.com>
2021-10-10 15:05:47 +0200

    refactor(capture): remove internal create method

--- src/payment-reference/_Capture.js
+++ src/payment-reference/_Capture.js
@@ -3,25 +3,11 @@ import { PaymobBase } from "../shared";
 /**
  * Creates an instance of Capture.
  * @param {*} secretKey
- * @memberof PaymobBase
+ * @memberof PaymobFactory
  */
     
 export class Capture extends PaymobBase {
 	constructor(secretKey) {
 		super(secretKey, "payment-reference/capture");
-	}
-
-	/**
-	 * create Capture.
-	 * @param {Object} data
-	 * @return {Promise} 
-	 * @memberof Capture
-	 */
-	create(data) {
-		const options = {
-			method: "post",
-			...this._getBasicOptions(data)
-		};
-		return this.request.request(options);
 	}
 }
