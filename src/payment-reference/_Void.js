d084ea04d5b5111a84570ae0b3aac30c4be145c6
Muhammed Moussa <mohamedmostafa@paymob.com>
2021-10-10 14:46:52 +0200

    refactor(void): remove internal create method

--- src/payment-reference/_Void.js
+++ src/payment-reference/_Void.js
@@ -3,25 +3,11 @@ import { PaymobBase } from "../shared";
 /**
  * Creates an instance of Void.
  * @param {*} secretKey
- * @memberof PaymobBase
+ * @memberof PaymobFactory
  */
     
 export class Void extends PaymobBase {
 	constructor(secretKey) {
 		super(secretKey, "payment-reference/void");
-	}
-
-	/**
-	 * create Void.
-	 * @param {Object} data
-	 * @return {Promise} 
-	 * @memberof Void
-	 */
-	create(data) {
-		const options = {
-			method: "post",
-			...this._getBasicOptions(data)
-		};
-		return this.request.request(options);
 	}
 }
