4e72a594764007006a2180acc69a3f4cb3e635bf
Muhammed Moussa <mohamedmostafa@paymob.com>
2021-10-10 14:30:26 +0200

    refactor(intention): extends PaymobBase and remove crud methods

--- src/intention/Intention.js
+++ src/intention/Intention.js
@@ -1,120 +1,18 @@
-import {
-	authHeaderBuilder,
-	HTTPRequest,
-	applicationJsonHeader
-} from "../http";
-import { getApiBaseUrl, getApiVersion } from "../utils";
-
-// @TODO: to be extendable from PaymobBase
+import { PaymobBase } from "../shared/PaymobBase";
 
 /**
  * Intention operations (create, update, patch, delete, retrieve, list)
  * @class Intention
  */
-export class Intention {
+export class Intention extends PaymobBase {
 	/**
 	 * Creates an instance of Intention.
 	 * @param {*} secretKey
 	 * @memberof PaymobFactory
 	 */
 	constructor(secretKey) {
+		super(secretKey, "intention");
 		this.secretKey = secretKey;
-		this.request = new HTTPRequest(this.secretKey);
-	}
-
-	/** @private */
-	_getApiBaseUrl() {
-		return getApiBaseUrl();
-	}
-
-	/** @private */
-	_getApiVersion() {
-		return getApiVersion();
-	}
-
-	/** @private */
-	_getFullUrl(route) {
-		const url = `${this._getApiBaseUrl()}/${this._getApiVersion()}/${route}/`;
-		return url;
-	}
-
-	/** @private */
-	_getBasicOptions(data) {
-		return {
-			url: this._getFullUrl("intention"),
-			body: data ? JSON.stringify(data) : {},
-			headers: {
-				...applicationJsonHeader(),
-				...authHeaderBuilder(this.secretKey)
-			}
-		};
-	}
-
-	/** @private */
-	_getBasicOptionsForGetRequests() {
-		return {
-			url: this._getFullUrl("intention"),
-			headers: {
-				...applicationJsonHeader(),
-				...authHeaderBuilder(this.secretKey)
-			}
-		};
-	}
-
-	/**
-	 * create intention.
-	 * @param {Object} data
-	 * @return {Promise} 
-	 * @memberof Intention
-	 */
-	create(data) {
-		const options = {
-			method: "post",
-			...this._getBasicOptions(data)
-		};
-		return this.request.request(options);
-	}
-
-	/**
-	 * update intention.
-	 * @param {Object} data
-	 * @return {Promise} 
-	 * @memberof Intention
-	 */
-	update(data) {
-		const options = {
-			method: "put",
-			...this._getBasicOptions(data)
-		};
-		return this.request.request(options);
-	}
-
-	/**
-	 * patch intention.
-	 * @param {Object} data
-	 * @return {Promise} 
-	 * @memberof Intention
-	 */
-	patch(data) {
-		const options = {
-			method: "patch",
-			...this._getBasicOptions(data)
-		};
-		return this.request.request(options);
-	}
-
-	/**
-	 * delete intention.
-	 * @param {Object} data
-	 * @return {Promise} 
-	 * @memberof Intention
-	 */
-	delete(data) {
-		const options = {
-			method: "delete",
-			...this._getBasicOptions(data)
-		};
-		return this.request.request(options);
 	}
 
 	/**
